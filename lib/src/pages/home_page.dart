0debfac6e75c43d36ac8f4991a04e0187ba43fd8
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-04-29 14:58:57 +0200

    Preparing search delegate

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -17,7 +17,9 @@ class HomePage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              // showSearch(context: context, delegate: delegate);
+            },
           )
         ],
       ),
---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Acciones del AppBar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del appbar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que vamos a mostrar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Sugerencias que aparecen cuando la persona escribe
+    throw UnimplementedError();
+  }
+}
