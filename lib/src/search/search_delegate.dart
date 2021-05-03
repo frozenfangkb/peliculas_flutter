fb307c071f509f70e46708da8efb7d60977a9f76
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-05-03 14:46:02 +0200

    App ready!!! 0.0.1

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,27 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class DataSearch extends SearchDelegate {
+  final peliculas = ["Spiderman", "Batman", "Aquaman"];
+
+  final peliculasRecientes = ["Spiderman", "Capitan America"];
+
+  final peliculasProvider = new PeliculasProvider();
+
+  String seleccion = "";
+
   @override
   List<Widget> buildActions(BuildContext context) {
-    // Acciones del AppBar
-    throw UnimplementedError();
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
-    // Icono a la izquierda del appbar
-    throw UnimplementedError();
+    return IconButton(
+        icon: AnimatedIcon(
+            icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+        onPressed: () {
+          close(context, null);
+        });
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // Crea los resultados que vamos a mostrar
-    throw UnimplementedError();
+    return Center(
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.redAccent,
+        child: Text(seleccion),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // Sugerencias que aparecen cuando la persona escribe
-    throw UnimplementedError();
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        if (snapshot.hasData) {
+          final peliculas = snapshot.data;
+
+          return ListView(
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                    image: NetworkImage(pelicula.getPosterImg()),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    width: 50,
+                    fit: BoxFit.contain),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  close(context, null);
+                  pelicula.uniqueId = "";
+                  Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                },
+              );
+            }).toList(),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
